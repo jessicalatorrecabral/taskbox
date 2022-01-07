7288e637de65d4b45a9d87a9d82565817e81fd5e
jessicalatorrecabral <jessica.latorre182@gmail.com>
2022-01-07 09:52:40 -0300

    webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 // .storybook/main.js
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
